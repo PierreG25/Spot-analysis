c2d0816d0af3232e04d29aa21ae45da6394a1c71
Pierre Gonot <gonot.pierre45@gmail.com>
2025-05-31 01:55:20 +0800

    clean data function

--- src/data_manipulation_func.py
+++ src/data_manipulation_func.py
@@ -6,4 +6,8 @@ def load_epex_data(filepath):
     """Function loading a csv file into a DataFrame"""
     return pd.read_csv(filepath)
 
+def clean_data(df):
+    """Function cleaning the data to make them exploitable"""
+    df.drop(['Area', 'Sequences', 'Intraday Period (CET/CEST)', 'Intraday Price (EUR/MWh)'], axis=1)
+    return df
 # End-of-file (EOF)

415ad3da28ce4f5ecfa62ec66a4bb45c9c302b26
Pierre Gonot <gonot.pierre45@gmail.com>
2025-06-01 16:07:18 +0800

    Data frame cleaning

--- src/data_manipulation_func.py
+++ src/data_manipulation_func.py
@@ -8,6 +8,12 @@ def load_epex_data(filepath):
 
 def clean_data(df):
     """Function cleaning the data to make them exploitable"""
-    df.drop(['Area', 'Sequences', 'Intraday Period (CET/CEST)', 'Intraday Price (EUR/MWh)'], axis=1)
+    df = df.drop(['Area', 'Sequence', 'Intraday Period (CET/CEST)', 'Intraday Price (EUR/MWh)'], axis=1)
+    df['MTU (CET/CEST)'] = df['MTU (CET/CEST)'].str.split(' - ').str[0]
+    df['MTU (CET/CEST)'] = pd.to_datetime(df['MTU (CET/CEST)'], format="%d/%m/%Y %H:%M:%S")
+    df['month'] = df['MTU (CET/CEST)'].dt.month
+    df['day'] = df['MTU (CET/CEST)'].dt.day
+    df['hour'] = df['MTU (CET/CEST)'].dt.hour
+    df = df[['MTU (CET/CEST)','month','day','hour','Day-ahead Price (EUR/MWh)']]
     return df
 # End-of-file (EOF)

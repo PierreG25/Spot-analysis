2a873a9345d047457c88e4b4ce44f8b1ec6cf369
Pierre Gonot <gonot.pierre45@gmail.com>
2025-06-05 01:29:31 +0800

    GitHub actions test 6

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -2,7 +2,6 @@ from src.plotting import process_time_period
 from datetime import date, datetime
 
 def test_process_time_period():
-    assert 1==0
     assert str(process_time_period('spring-2024')) == (date(2024,3,20), date(2024,6,20))
     assert process_time_period('summer-2024') == (date(2024,6,21), date(2024,9,22))
     assert process_time_period('autumn-2024') == (date(2024,9,23), date(2024,12,20))

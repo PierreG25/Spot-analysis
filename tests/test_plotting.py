db6bf3f2bfad24a581d76eb609addf65c202f6a3
Pierre Gonot <gonot.pierre45@gmail.com>
2025-06-05 16:50:09 +0800

    pre-commit test 1

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -2,8 +2,7 @@ from src.plotting import process_time_period
 from datetime import date, datetime
 
 def test_process_time_period():
-    assert str(process_time_period('spring-2024')) == (date(2024,3,20), date(2024,6,20))
+    assert process_time_period('spring-2024') == (date(2024,3,20), date(2024,6,20))
     assert process_time_period('summer-2024') == (date(2024,6,21), date(2024,9,22))
     assert process_time_period('autumn-2024') == (date(2024,9,23), date(2024,12,20))
     assert process_time_period('winter-2024') == (date(2024,12,21), date(2025,3,19))
-    assert 1==0
